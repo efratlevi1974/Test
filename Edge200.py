01fa80acd2c7f57a4c352cfe0d54ad5d81c00625
efratlevi1974 <efrat.levi1974@gmail.com>
2018-11-25 19:03:17 +0200

    ggg comment

---
+++ Edge200.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+#driver=webdriver.Chrome(executable_path="/home/efat/Documents/DevOps/Selenium/chromedriver")
+driver=webdriver.Firefox(executable_path="/home/efat/Documents/DevOps/Selenium/geckodriver")
+
+driver.get('https://eu-innovi-staging.agentvi.com')
+driver.find_element_by_name("email").send_keys("admin@agentvi.com")
+driver.find_element_by_name("password").send_keys("12345678")
+driver.find_element_by_class_name("btn-lg").click()
+driver.find_element_by_id("devices").click()
+
+#efratjgggfff
+
+print(driver.current_url)
+# print(driver.title)
+# print(driver.page_source)
+
+#driver.find_element_by_id("cameraDashboard").click()
+
+
+# driver.refresh()
+#
+# driver.quit()
